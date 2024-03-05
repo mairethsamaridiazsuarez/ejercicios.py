7b49a19c67ee3170c5705074fd3870cf63ac4857
mairethsamaridiazsuarez <160806821+mairethsamaridiazsuarez@users.noreply.github.com>
2024-03-05 15:11:27 -0500

    aqui esta el saludo

---
+++ promedio.py
@@ -0,0 +1,6 @@
+num1 = float (input('ingrese su primer nota : '))
+num2 = float(input('ingrese su segunda nota : '))
+num3 = float(input('ingrese su tercer nota : '))
+num4 = float(input('ingrese su cuarta nota : '))
+promedio =  (num1 + num2 + num3 + num4) / 4
+print('Su promedio es : ' , promedio )
---
+++ saludo.py
@@ -0,0 +1,4 @@
+nombre = input('ingrese su nombre : ')
+mensaje = 'bienvenido ' + nombre
+print (mensaje)
+
